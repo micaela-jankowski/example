593ee5b9cdd382cfc5a3e41f01bde1459bbdf77b
micaela-jankowski <micaelajankowski@gmail.com>
2025-02-26 01:41:11 -0800

    use pipeline file instead of BK ui, removed docker plugin

--- .buildkite/execution.sh
+++ .buildkite/execution.sh
@@ -3,5 +3,4 @@
 buildkite-agent artifact download hello/my-hello-app . --step build-step
 cd hello
 chmod +x ./my-hello-app
-echo "Hello, $(buildkite-agent meta-data get name)!"
 ./my-hello-app "$(buildkite-agent meta-data get name)"
